e7fd6c70ee27f975ccccfba8557d29d207f25995
Dmytro Markuntovych <sunsn4ke@gmail.com>
2024-01-23 17:49:21 +0200

    feat: implement delete func

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { access, unlink } from 'node:fs';
+
 const remove = async () => {
-    // Write your code here 
+  const path = 'src/fs/files/fileToRemove.txt';
+
+  access(path, (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      unlink(path, (error) => {
+        if (err) {
+          console.error(error);
+        } else {
+          console.log('File was deleted');
+        }
+      })
+    }
+  })
 };
 
 await remove();
