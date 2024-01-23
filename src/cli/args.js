af4cbe92ff706c7865e0e00e0b750728427210d4
Dmytro Markuntovych <sunsn4ke@gmail.com>
2024-01-23 19:38:30 +0200

    feat: implement cli args func

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+  const result = argv.slice(2)
+    .reduce((acc, value, index) =>
+      index % 2 === 0 ? acc + value.slice(2) + ' is ' : acc + value + ', ', '');
+
+  console.log(result.slice(0, -2));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,6 +1,8 @@
+import { env } from 'node:process';
+
 const parseEnv = () => {
   const prefix = 'RSS_';  
-  const RSSVariables = Object.entries(process.env)
+  const RSSVariables = Object.entries(env)
     .reduce((acc, [key, value]) =>
       key.startsWith(prefix) ? acc + `${key}=${value}; ` : acc, '');
 
