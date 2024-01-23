f1034a56afd63fc46bb3ec1222921514e152c139
Dmytro Markuntovych <sunsn4ke@gmail.com>
2024-01-23 18:11:25 +0200

    feat: implement fs rename func

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,31 @@
+import { access, rename as renameFile } from 'node:fs';
+
 const rename = async () => {
-    // Write your code here 
+  const path = 'src/fs/files/';
+  const oldFile = 'wrongFilename.txt';
+  const newFile = 'properFilename.md';
+  const oldFilePath = path + oldFile;
+  const newFilePath = path + newFile;
+  
+  access(oldFilePath, (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      access(newFilePath, (err) => {
+        if (err) {
+          renameFile(oldFilePath, newFilePath, (error) => {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('File was renamed');
+            }
+          })
+        } else {
+          throw new Error('FS operation failed');
+        }
+      })
+    }
+  })
 };
 
 await rename();
