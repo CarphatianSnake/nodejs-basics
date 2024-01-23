de71d03eca6754f654903d4c0042c2bd606fb038
Dmytro Markuntovych <sunsn4ke@gmail.com>
2024-01-23 18:25:48 +0200

    feat: implement fs copy func

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import { access, cp } from 'node:fs';
+
 const copy = async () => {
-    // Write your code here 
+  const path = 'src/fs/';
+  const dir = path + 'files';
+  const newDir = path + 'files_copy';
+
+  access(dir, (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      access(newDir, (err) => {
+        if (err) {
+          cp(dir, newDir, { recursive: true }, (error) => {
+            if (error) {
+              console.error(error);
+            } else {
+              console.log('Folder copied successfully');
+            }
+          });
+        } else {
+          throw new Error('FS operation failed');
+        }
+      })
+    }
+  })
 };
 
 await copy();
