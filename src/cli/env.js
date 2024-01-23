efaae54c444bc6754a9b0dded29d2392ed432f72
Dmytro Markuntovych <sunsn4ke@gmail.com>
2024-01-23 19:02:03 +0200

    feat: implement cli env func

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = 'RSS_';  
+  const RSSVariables = Object.entries(process.env)
+    .reduce((acc, [key, value]) =>
+      key.startsWith(prefix) ? acc + `${key}=${value}; ` : acc, '');
+
+  console.log(RSSVariables.slice(0, -2));
 };
 
 parseEnv();
