848441f540e64f5b2befe5d0e03597784a1bfe40
Dmytro Markuntovych <sunsn4ke@gmail.com>
2024-01-24 02:50:18 +0200

    feat: implement calcHahs func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  const input = createReadStream('src/hash/files/fileToCalculateHashFor.txt');
+
+  input.on('readable', () => {
+    const chunk = input.read();
+
+    if (chunk) {
+      hash.update(chunk);
+    } else {
+      console.log(`File's hash: ${hash.digest('hex')}`);
+    }
+  })
 };
 
 await calculateHash();
