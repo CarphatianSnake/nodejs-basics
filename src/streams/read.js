2cc83410baecb6035e45c26472e6a3dc5a68adf1
Dmytro Markuntovych <sunsn4ke@gmail.com>
2024-01-24 18:25:20 +0200

    feat: implement stream read func using fs.createReadStream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+  const path = 'src/streams/files/fileToRead.txt';
+  const encoding = 'utf8';
+  const stream = createReadStream(path, { encoding });
+
+  stream.on('data', (chunk) => {
+    stdout.write(chunk);
+  })
 };
 
 await read();
