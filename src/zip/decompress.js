4047ff6fcd4074edb378e882f3eba8c3313b2e10
Dmytro Markuntovych <sunsn4ke@gmail.com>
2024-01-29 18:11:09 +0200

    feat: implement zip decompress func

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { join } from 'node:path';
+import { createUnzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { checkAccess } from '../fs/utils/checkAccess.js';
+import { throwError } from '../fs/utils/throwError.js';
+
 const decompress = async () => {
-  // Write your code here 
+  const filesDir = join('src', 'zip', 'files');
+  const fileToDecompress = join(filesDir, 'archive.gz');
+  const decompressedFile = join(filesDir, 'fileToCompress.txt');
+
+  const callback = () => {
+    const decompress = createUnzip();
+    const input = createReadStream(fileToDecompress);
+    const output = createWriteStream(decompressedFile);
+  
+    input.pipe(decompress).pipe(output);
+
+    console.log('File decompressed successfully!');
+  }
+
+  checkAccess(decompressedFile, throwError, callback);
 };
 
 await decompress();
